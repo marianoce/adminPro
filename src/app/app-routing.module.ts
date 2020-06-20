30d055904bb8b3c88d939b909f3dcbf9e1dff09e
Mariano <mariano.ce@gmail.com>
2020-06-20 20:51:37 -0300

    Modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,6 @@ import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
-  { path: '', component: PagesComponent, children: [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'progress', component: ProgressComponent },
-    { path: 'graficas1', component: Graficas1Component },
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-  ] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent }
